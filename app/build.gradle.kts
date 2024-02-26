e77491f16dcf44bba1b97bf5081b79252565bf0f
mccThai <tqthai1511@gmail.com>
2024-02-26 15:19:24 +0700

    setup gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.4.3"
     }
     packaging {
         resources {
@@ -58,11 +58,11 @@ dependencies {
     implementation(libs.bundles.androidComposeLibs)
     implementation(libs.bundles.googleAccompanistLibs)
 
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation(libs.compose.ui.ui.test.junit4)
-    debugImplementation(libs.compose.ui.ui.tooling)
-    debugImplementation(libs.compose.ui.test.manifest)
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
